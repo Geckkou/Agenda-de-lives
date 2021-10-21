e0902f48c563cc90b8861a83b5b7a4c00f1afe0a
Geckkou <alexandre.asr2001@hotmail.com>
2021-10-21 19:31:13 -0300

    cart and creating model

---
+++ src/app/shared/model/live.model.ts
@@ -0,0 +1,29 @@
+import { SafeResourceUrl } from '@angular/platform-browser';
+
+class Live {
+    id: string;
+    liveName: string;
+    channelName: string;
+    liveDate: string;
+    liveTime: string;
+    liveLink: string;
+    registrationDate: string;
+    urlSafe: SafeResourceUrl;
+
+    constructor(id?: string, liveName?: string, channelName?: string, liveDate?: string,
+      liveTime?: string, liveLink?: string, registrationDate?: string, urlSafe?: SafeResourceUrl) {
+
+        this.id = id || '';
+        this.liveName = liveName || '';
+        this.channelName = channelName || '';
+        this.liveDate = liveDate || '';
+        this.liveTime = liveTime || '';
+        this.liveLink = liveLink || '';
+        this.registrationDate = registrationDate || '';
+        this.urlSafe = urlSafe || '';
+      }
+  }
+
+  export default Live;
+
+
